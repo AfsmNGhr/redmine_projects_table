d9111217f2e4657fdbc0f317c44f245b260c2921
Application deployer <deployer@freefreight.biz>
2015-08-10 09:32:02 -0400

    initial commit ...

---
+++ init.rb
@@ -0,0 +1,12 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_org_projects_table do
+  name 'Redmine Org projects table'
+  author 'Ermolaev Alexsey'
+  description 'Table projects with customizable and searchable fields ...'
+  author_url 'mailto:afay.zangetsu@gmail.com'
+  version '0.1'
+  requires_redmine version_or_higher: '3.0.0'
+  requires_redmine_plugin :redmine_base_deface, version_or_higher: '0.0.1'
+  requires_redmine_plugin :redmine_organizations, version_or_higher: '0.5'
+end
