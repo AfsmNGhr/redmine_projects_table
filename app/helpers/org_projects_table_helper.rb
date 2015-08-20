a3e5e40240b31fb072c34ed942a964b07ad27d0e
Application deployer <deployer@freefreight.biz>
2015-08-20 10:05:59 -0400

    last changes ... two columns ...

---
+++ app/helpers/org_projects_table_helper.rb
@@ -0,0 +1,73 @@
+# coding: utf-8
+module OrgProjectsTableHelper
+  def issue_links(project)
+    {
+      all: {
+        caption: project.issues.count,
+        class: 'issues-all',
+        label: l(:label_issues_all),
+        path: "/issues?utf8=✓&set_filter=1&f[]=status_id&op[status_id]=*&f[]=project_id&op[project_id]==&v[project_id][]=#{project.id}",
+        id: "org-table-issues-all-#{project.id}"
+      },
+
+      close: {
+        caption: project.issues.where(status_id: 3).count,
+        class: 'issues-close',
+        label: l(:label_issues_close),
+        path: "/issues?utf8=✓&set_filter=1&f[]=status_id&op[status_id]==&v[status_id][]=3&f[]=project_id&op[project_id]==&v[project_id][]=#{project.id}",
+        id: "org-table-issues-close-#{project.id}"
+      },
+
+      open: {
+        caption: project.issues.where(status_id: 1).count,
+        class: 'issues-open',
+        label: l(:label_issues_open),
+        path: "/issues?utf8=✓&set_filter=1&f[]=status_id&op[status_id]==&v[status_id][]=1&f[]=project_id&op[project_id]==&v[project_id][]=#{project.id}",
+        id: "org-table-issues-open-#{project.id}"
+      },
+
+      add: {
+        caption: 'add',
+        class: 'material-icons',
+        label: l(:label_issue_new),
+        path: new_project_issue_path(project),
+        id: "org-table-issue-add-#{project.id}"
+      }
+    }
+  end
+
+  def org_issues_column(project, column=[], links=issue_links(project))
+    links.keys.each do |key|
+      column << org_issues_node(project, links[key])
+    end
+    column.empty? ? nil : column.join("\n").html_safe
+  end
+
+  def org_issues_node(project, link)
+    link_to (content_tag(:span, link[:caption], class: link[:class]) +
+             (content_tag(:span, link[:label], class: 'mdl-tooltip',
+                          for: link[:id]))), link[:path], id: link[:id]
+  end
+
+  def org_organizations_column(project, column=[])
+    project.organizations.each do |org|
+      column << org_organizations_node(org, project)
+    end
+    column.empty? ? nil : column.join("\n").html_safe
+  end
+
+  def org_organizations_node(org, project)
+    roles = org.organization_roles.map { |r| r.role.name }.compact.uniq
+    if roles.empty?
+      content_tag(:span, content_tag(:span, (link_to_organization org)))
+    else
+      content_tag(:span) do
+        link_to (content_tag(:span, org.fullname) +
+                 (content_tag(:span, roles.join("<br>"), class: 'mdl-tooltip',
+                              for: "org-table-project-#{project.id}-organization-#{org.id}"))),
+                organization_path(org),
+                id: "org-table-project-#{project.id}-organization-#{org.id}"
+      end
+    end
+  end
+end
--- init.rb
+++ init.rb
@@ -1,12 +1,18 @@
 require 'redmine'
-require 'redmine_org_projects_table/patches/projects_controller_patch'
+
+ActionDispatch::Callbacks.to_prepare do
+  require 'redmine_org_projects_table/patches/projects_controller_patch'
+  ActionView::Base.send(:include, OrgProjectsTableHelper)
+end
 
 Redmine::Plugin.register :redmine_org_projects_table do
   name 'Redmine Org projects table'
   author 'Ermolaev Alexsey'
   description 'Table projects with customizable and searchable fields ...'
+  url 'git@freefreight.biz:~/redmine/org_projects_table'
   author_url 'mailto:afay.zangetsu@gmail.com'
   version '0.1'
   requires_redmine version_or_higher: '3.0.0'
-  requires_redmine_plugin :redmine_base_deface, version_or_higher: '0.0.1'
+  # requires_redmine_plugin :redmine_organizations, version_or_higher: '0.5'
+  # requires_redmine_plugin :redmine_material_theme, version_or_higher: '0.1'
 end
